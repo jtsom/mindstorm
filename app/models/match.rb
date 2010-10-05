1762290fed16839620a7622df3a8bca58d8ec8c8
John <johnts@charter.net>
2010-10-04 23:18:22 -0400

    Updated validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,9 +2,10 @@ class Match < ActiveRecord::Base
    
     serialize :results
     
-    validates_presence_of :match_number, :message => "can't be blank"
-    validates_presence_of :table_number, :message => "can't be blank"
-    validates_uniqueness_of :match_number, :scope => [:team_id, :type], :message => "for this team already exists"
+    #validates_presence_of :match_number, :message => "can't be blank"
+    validates :match_number, :presence => true #, :message  => "can not be blank"
+    validates :table_number, :presence => true
+    validates :match_number, :uniqueness => {:scope => [:team_id, :type] }
     
     validates_exclusion_of :table_number, :in => [1..APP_CONFIG["max_matches"]], :message => "between 1 and #{APP_CONFIG["max_matches"]}"
     
