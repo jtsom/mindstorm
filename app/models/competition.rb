ce39355966d6920925431c7281f3fdcef4b991bc
John <johnts@charter.net>
2011-10-26 20:47:06 -0400

    updated competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,8 +2,8 @@ class Competition < ActiveRecord::Base
   has_many :teams
   
   def self.authenticate(competition_name, password)
-    competition = find_by_name(competition_name)
-    if competition && competition.password == password
+    competition = find_by_name(competition_name.downcase)
+    if competition && competition.password.downcase == password.downcase
       competition
     else
       nil
