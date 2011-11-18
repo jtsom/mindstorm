58683ec72593b15fee832d0032428cd52f3af515
John <johnts@charter.net>
2011-11-17 19:23:35 -0500

    updated mailer

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -7,6 +7,9 @@ class TeamMailer < ActionMailer::Base
       @competition = competition
       to_email_with_name = "#{team.coach} <#{team.coach_email}>"
       from_email_with_name = "#{competition.full_name} <#{competition.from_email}>"
-      mail(:from => from_email_with_name, :to => to_email_with_name, :subject => "Your Team's Results at #{competition.full_name}")
+      mail(:from => competition.from_email, :cc => competition.from_email, 
+            :reply_to => from_email_with_name,
+            :to => to_email_with_name,
+            :subject => "Your Team's Results at #{competition.full_name}")
     end
 end
