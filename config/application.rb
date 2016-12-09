0750ef5425cf7386f854309ad7fed8c7d3820c17
John Tsombakos <johnt519@gmail.com>
2016-12-09 06:12:53 -0500

    Add x-frame-options to app config

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module Mindstorm
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/assets/fonts"
+
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
