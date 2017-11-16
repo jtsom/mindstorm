653b41f0a2530410e92dce5f3e2c35c386fb20ff
John Tsombakos <johnt519@gmail.com>
2017-11-15 19:25:02 -0500

    fix upload

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -113,7 +113,7 @@ class TeamsController < ApplicationController
 
     teams = JSON.parse(raw)
     puts @current_competition.id
-    Team.destroy_all(competition_id: @current_competition.id)
+    Team.where(competition_id: @current_competition.id).destroy_all
 
     if teams.count > 0
       teams.each { |team|
