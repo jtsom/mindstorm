56597fcaf76e4b6c25524e46b525437f490f2182
John Tsombakos <johnt519@gmail.com>
2017-11-17 13:30:00 -0500

    Fix label on M11 new pipe mat contact

--- config/initializers/challenge-2017.rb
+++ config/initializers/challenge-2017.rb
@@ -236,7 +236,7 @@ challenge do
     end
 
   mission "M10 Pipe Replacement" do
-    item :new_pipe_installed, "New Pipe is installed where Broken Pipe was?", "", ["Yes", "No"], ["1", "0"]
+    item :new_pipe_installed, "New Pipe is installed where Broken Pipe was?", "0", ["Yes", "No"], ["1", "0"]
     item :new_pipe_mat_contact, "This New Pipe has full/flat contact with the mat?", "20", ["Yes", "No"], ["1", "0"]
     score do |items|
        (items[:new_pipe_installed].to_i * 1) * (items[:new_pipe_mat_contact].to_i * 20)
@@ -244,11 +244,11 @@ challenge do
   end
 
   mission "M11 Pipe Construction" do
-    item :new_pipe_mat_contact, "New Pipe has full/flat contact with the mat?", "12", ["Yes", "No"], ["1", "0"]
+    item :new_pipe_mat_contact_11, "New Pipe has full/flat contact with the mat?", "12", ["Yes", "No"], ["1", "0"]
     item :new_pipe_partial, "This new pipe is partially in its target?", "15", ["Yes", "No"], ["1", "0"]
     item :new_pipe_complete, "This new pipe is completely in its target?", "20", ["Yes", "No"], ["1", "0"]
     score do |items|
-      ((items[:new_pipe_mat_contact].to_i) * 1) * (((items[:new_pipe_partial].to_i) * 15) + ((items[:new_pipe_complete].to_i) * 20))
+      ((items[:new_pipe_mat_contact_11].to_i) * 1) * (((items[:new_pipe_partial].to_i) * 15) + ((items[:new_pipe_complete].to_i) * 20))
     end
     check "New Pipe can only be PARTIALLY or COMPLETELY in its target" do |items|
       (items[:new_pipe_partial].to_i + items[:new_pipe_complete].to_i) <= 1
