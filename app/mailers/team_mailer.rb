a22545b287997b6f98844349b970b4ab9093c2fd
John Tsombakos <johnt519@gmail.com>
2019-11-26 23:16:55 -0500

    mail problems

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -1,6 +1,8 @@
 class TeamMailer < ActionMailer::Base
-    
+
     def team_details_email(competition, team, qualifications, finals)
+      puts ENV['MINDSTORM_EMAIL_LOGIN']
+      puts ENV['MINDSTORM_EMAIL_PASSWORD']
       @team = team
       @qualifications = qualifications
       @finals = finals
@@ -17,7 +19,7 @@ class TeamMailer < ActionMailer::Base
       @team.corevalue_scores.each do |score|
         @c_count += score.award_count
       end
-      
+
       cc_email = competition.from_email
       if team.asst_coach_email != nil && team.asst_coach_email != ''
         cc_email += ', ' + team.asst_coach_email
@@ -25,7 +27,7 @@ class TeamMailer < ActionMailer::Base
 
       to_email_with_name = "#{team.coach} <#{team.coach_email}>"
       from_email_with_name = "#{competition.contact_name} <#{competition.from_email}>"
-      mail(from: competition.from_email, cc: competition.from_email, 
+      mail(from: competition.from_email, cc: competition.from_email,
             reply_to: from_email_with_name,
             to: to_email_with_name,
             subject: "Your Team's Results at #{competition.full_name}")
