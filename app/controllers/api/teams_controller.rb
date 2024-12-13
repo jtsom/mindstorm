1b37cfe2c9f6b92d811387843e58829b30eabc63
John Tsombakos <johnt519@gmail.com>
2024-12-13 18:12:12 -0500

    Add ids to apis; remove core values links

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -4,14 +4,14 @@ module Api
 
 		def index
 			teams = Team.includes(:qualifications).where("competition_id = ?", params[:competition_id]).order(:fll_number)
-			respond_with teams.to_json(only: [:fll_number, :team_name, :coach, :town], include: [qualifications: {}])
+			respond_with teams.to_json(only: [:fll_number, :team_name, :coach, :town, :id], include: [qualifications: {}])
 		end
 
 		def show
 			team = Team.includes(:qualifications).find(params[:id])
 			x = team.to_json(only: [:fll_number, :team_name, :coach, :town], include: [qualifications: {}])
 			# respond_with team.as_json(only: [:fll_number, :team_name, :coach, :town, :qualifications]), include: {qualifications: {}}
-			respond_with team.to_json(only: [:fll_number, :team_name, :coach, :town], include: [qualifications: {}])
+			respond_with team.to_json(only: [:fll_number, :team_name, :coach, :town, :id], include: [qualifications: {}])
 		end
 
 	end
